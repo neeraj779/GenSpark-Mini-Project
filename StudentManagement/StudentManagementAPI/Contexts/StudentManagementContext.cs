ec28c9858ada937a082bb402715b3486cfcc88e5
Neeraj <neeraj79108@gmail.com>
2024-05-24 10:54:02 +0530

    chore: Add StudentManagementContext class and configure database models

---
+++ StudentManagement/StudentManagementAPI/Contexts/StudentManagementContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagementAPI.Models.DBModels;
+
+public class StudentManagementContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {}
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Degree> Degrees { get; set; }
+    public DbSet<Branch> Branches { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Enrollment> Enrollments { get; set; }
+    public DbSet<ClassAttendance> ClassAttendances { get; set; }
+    public DbSet<Assignment> Assignments { get; set; }
+    public DbSet<Submission> Submissions { get; set; }
+    public DbSet<Teacher> Teachers { get; set; }
+    public DbSet<CourseOffering> CourseOfferings { get; set; }
+    public DbSet<Class> Classes { get; set; }
+}
