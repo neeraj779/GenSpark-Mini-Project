ba5b5ae9f3c33c137c4a157d2b445f1fec4f6d4e
Neeraj <neeraj79108@gmail.com>
2024-05-30 09:42:06 +0530

    feat: Add phone number validation to DTOs

--- StudentManagement/StudentManagementAPI/Models/DTOs/TeacherRegisterDTO.cs
+++ StudentManagement/StudentManagementAPI/Models/DTOs/TeacherRegisterDTO.cs
@@ -14,6 +14,7 @@ namespace StudentManagementAPI.Models.DTOs
         public DateTime DateOfBirth { get; set; }
 
         [MaxLength(10, ErrorMessage = "Phone number cannot exceed 10 characters")]
+        [MinLength(10, ErrorMessage = "Phone number must be at least 10 characters")]
         [DataType(DataType.PhoneNumber)]
         [Phone(ErrorMessage = "Invalid phone number.")]
         public string Phone { get; set; } = string.Empty;
